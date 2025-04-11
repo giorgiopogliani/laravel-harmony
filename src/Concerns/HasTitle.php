480efbedadcfd0eeae9f4abb67e7048b9021f7eb
Giorgio Pogliani <giorgiopogliani94@gmail.com>
2025-04-11 12:26:45 +0200

    Update HasTitle.php

--- src/Concerns/HasTitle.php
+++ src/Concerns/HasTitle.php
@@ -6,14 +6,14 @@ namespace Performing\Harmony\Concerns;
 
 trait HasTitle
 {
-    public function __construct(string $title = null)
+    public function __construct(?string $title = null)
     {
         parent::__construct();
 
         $this->data['title'] = $title;
     }
 
-    public static function make(string $title = null)
+    public static function make(?string $title = null)
     {
         return new static($title);
     }
