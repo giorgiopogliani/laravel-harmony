d4ae9e78eec5ba9d757c32df76db5514b915b897
Giorgio Pogliani <giorgiopogliani94@gmail.com>
2026-03-09 19:24:45 +0100

    Create Prop.php

---
+++ src/Prop.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Performing\Harmony;
+
+#[\Attribute]
+class Prop
+{
+    public function __construct(
+        public ?string $key = null,
+    ) {}
+}
