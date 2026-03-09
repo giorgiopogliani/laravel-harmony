e742dd2e0a3e28ba71def86c59e7d992c0b75001
Giorgio Pogliani <giorgiopogliani94@gmail.com>
2026-03-09 22:01:54 +0100

    Update ActionsColumn.php

--- src/Columns/ActionsColumn.php
+++ src/Columns/ActionsColumn.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Performing\Harmony\Columns;
 
+use Closure;
 use Performing\Harmony\Contracts\Column;
 use Performing\Harmony\Contracts\RenderType;
 use Illuminate\Support\Str;
 use Override;
+use Performing\Harmony\Components\Link;
 use Performing\Harmony\RenderTypes\ActionsRenderType;
 
 /**
@@ -19,11 +21,13 @@ final class ActionsColumn implements Column
 {
     /**
      * @param  class-string<T>  $base
+     * @param  Closure(T): array<string, Link> $links
      */
     public function __construct(
         public string $base,
         public string $name = 'Text',
         public ?string $key = null,
+        public Closure $links = [],
     ) {}
 
     #[Override]
@@ -48,7 +52,7 @@ final class ActionsColumn implements Column
     /** @param T $model */
     public function value(mixed $model): mixed
     {
-        return data_get($model, $this->key());
+        return call_user_func($this->links, $model);
     }
 
     #[Override]
