036835af3601d5e728ebca35d23af068026f98ff
Giorgio Pogliani <giorgiopogliani94@gmail.com>
2026-03-10 14:51:59 +0100

    Create HasTitle.php

---
+++ src/Concerns/HasTitle.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Performing\Harmony\Concerns;
+
+trait HasTitle
+{
+    public function __construct(?string $title = null)
+    {
+        $this->data['title'] = $title;
+
+        parent::__construct();
+    }
+
+    public static function make(?string $title = null)
+    {
+        return new static($title);
+    }
+
+    public function getTitle(): string
+    {
+        return $this->data['title'];
+    }
+}
